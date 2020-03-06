949197eb8a761bb19dc4673bc34026c032cef04c
Amanda Kaufman <aka@microsoft.com>
2020-03-06 13:37:00 -0700

    Initial commit

---
+++ CL5GithubDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CL5GithubDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to our CL5 Github Workshop.");
+        }
+    }
+}
